5d673064eb1629eaeb786d9f21f9a354c55a9271
Atish Jain <atish_jain@yahoo.com>
2023-06-19 09:53:21 +0530

    Add files via upload
    This this program contains program logic about bill amount calculation

---
+++ biodata.c
@@ -0,0 +1,11 @@
+//Program to Print biodata
+
+# include <stdio.h>
+main()
+{
+    printf("Name: Atish Jain");
+    printf("\nQualification:MCA, Msc(IT), MSCS(UK,London)");
+    printf("\nCity: Rajahmundry");
+
+    printf("\n");
+}
